e5e07ce3d993f5337ed5709b11a25eb6076b1b02
d.gorbunov <d.gorbunov@2gis.ru>
2022-03-04 21:03:36 +0700

    Изменены имена переменных в скрипте запуска postgres

--- run_postgres.sh
+++ run_postgres.sh
@@ -2,6 +2,6 @@ docker run --rm -d \
     --name postgres \
     -p 5432:5432 \
     -v ${HOME}/postgresql/data:/var/lib/postgresql/data \
-    -e POSTGRES_USER=${POSTGRES_USER} \
-    -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+    -e DB_USER=${DB_USER} \
+    -e DB_PASSWORD=${DB_PASSWORD} \
     postgres:13
