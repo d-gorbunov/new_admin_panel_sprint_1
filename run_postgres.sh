2648187aceed0eeea66fac3f2c4a80dfd67a6573
Daniil Gorbunov <d.gorbunov0312@gmail.com>
2022-03-06 14:44:37 +0700

    Изменён скрипт запуска postgres

--- run_postgres.sh
+++ run_postgres.sh
@@ -1,7 +1,8 @@
-docker run --rm -d \
+docker run -d --rm \
     --name postgres \
     -p 5432:5432 \
     -v ${HOME}/postgresql/data:/var/lib/postgresql/data \
-    -e DB_USER=${DB_USER} \
-    -e DB_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     postgres:13
