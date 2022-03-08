a3530a7c75f7f10ec0dca1206e3d0b7778ea8cca
Daniil Gorbunov <d.gorbunov0312@gmail.com>
2022-03-08 13:14:00 +0700

    Переписаны датаклассы

--- 03_sqlite_to_postgres/data_classes.py
+++ 03_sqlite_to_postgres/data_classes.py
@@ -5,45 +5,46 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Common:
-    __slots__ = ('id', 'created', 'modified')
     id: uuid.UUID
-    created: datetime
-    modified: datetime
+    created_at: datetime
 
 
 @dataclass(frozen=True)
 class FilmWork(Common):
-    __slots__ = ('title', 'description', 'creation_date', 'rating', 'type')
+    __slots__ = ('id', 'title', 'description', 'creation_date', 'rating', 'type', 'created_at', 'updated_at')
     type: str
     title: str
     rating: float
     description: str
+    updated_at: datetime
     creation_date: datetime
 
 
 @dataclass(frozen=True)
 class Genre(Common):
-    __slots__ = ('name', 'description')
+    __slots__ = ('id', 'name', 'description', 'created_at', 'updated_at')
     name: str
     description: str
+    updated_at: datetime
 
 
 @dataclass(frozen=True)
 class Person(Common):
-    __slots__ = ('full_name',)
+    __slots__ = ('id', 'full_name', 'created_at', 'updated_at')
     full_name: str
+    updated_at: datetime
 
 
 @dataclass(frozen=True)
 class GenreFilmWork(Common):
-    __slots__ = ('genre_id', 'film_work_id')
+    __slots__ = ('id', 'genre_id', 'film_work_id', 'created_at')
     genre_id: uuid.UUID
     film_work_id: uuid.UUID
 
 
 @dataclass(frozen=True)
 class PersonFilmWork(Common):
-    __slots__ = ('role', 'person_id', 'film_work_id')
+    __slots__ = ('id', 'person_id', 'film_work_id', 'role', 'created_at')
     role: str
-    person_id: uuid.UUID
-    film_work_id: uuid.UUID
+    person_id: datetime
+    film_work_id: datetime
