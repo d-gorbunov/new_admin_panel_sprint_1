51d5fddecd42a9e88c166f26da9c8c241c149ec8
Daniil Gorbunov <d.gorbunov0312@gmail.com>
2022-03-09 20:21:46 +0700

    Файл settings.py разбит на модули

---
+++ 02_movies_admin/config/components/installed_apps.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_admin_listfilter_dropdown',
+    'movies'
+]
